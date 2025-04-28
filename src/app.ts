a32c8e9c457332f00a2a025c720cf7789857c0c0
James Cole <jcole2@midwesttape.com>
2025-04-28 16:13:08 -0400

    add some notes

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,21 @@
-import express, {Express, Router} from 'express'
+import express, {Router} from 'express'
 
 export async function createExpressApp() {
-    const app = express()
+    // create router so we can create routes for our API
     const router = Router()
 
+    // create a route that returns a simple JSON response
     router.get('/', async (req, res) => {
         res.status(200).json({message: 'Hello world'})
     })
 
+    // create the express app
+    const app = express()
+
+    // set a larger size payload allotment
     app.use(express.json({limit: '100mb'}))
 
+    // register the routes made above
     app.use('/api', router)
 
     return app
